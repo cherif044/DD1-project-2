f18f59eac0eb413c0b3e051be218c90f39b0b9e8
Saeedmohameds <144555221+Saeedmohameds@users.noreply.github.com>
2024-12-01 23:44:06 +0200

    Update topmodule.v

--- topmodule.v
+++ topmodule.v
@@ -1,42 +1,40 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
 
 module topmodule(
-    input clk_100MHz,     
-    input reset,          
-    input [2:0] sw,        
-    output hsync,          
-    output vsync,         
-    output [3:0] vga_red,  
-    output [3:0] vga_green,
-    output [3:0] vga_blue 
-);
-
-    wire video_on;
-    wire p_tick;
-    wire [9:0] x, y;
-
-    vga_sync vga_sync_inst (
-        .clk_100MHz(clk_100MHz),
-        .reset(reset),
-        .video_on(video_on),
-        .hsync(hsync),
-        .vsync(vsync),
-        .p_tick(p_tick),
-        .x(x),
-        .y(y)
+    input clk100MHz,        // from Basys 3
+    input reset,            // btnR
+    input up,               // btnU
+    input down,
+    input up2,
+    input down2,             // btnD
+    output hsync,           // to VGA port
+    output vsync,           // to VGA port
+    output [11:0] rgb     // to DAC, to VGA port
+//    output [6:0] 
     );
-
-
-    pixels pixels_inst (
-        .clk_100MHz(clk_100MHz),
-        .reset(reset),
-        .sw(sw),
-        .video_on(video_on),
-        .red(vga_red),
-        .green(vga_green),
-        .blue(vga_blue)
-    );
-
+    
+    wire wReset, wUp, wDown, wVidOn, wPTick,wUp2,wDown2;
+    wire [9:0] wX, wY;
+    reg [11:0] rgbReg;
+    wire [11:0] rgbNext;
+    
+    vga_sync vga(.clk100MHz(clk100MHz), .reset(wReset), .videoOn(wVidOn),
+                      .hsync(hsync), .vsync(vsync), .pTick(wPTick), .x(wX), .y(wY));
+    pixels pg(.clk(clk100MHz), .reset(wReset), .up(wUp), .down(wDown), 
+                 .videoOn(wVidOn), .xCoordinate(wX), .yCoordinate(wY), . rgbColor(rgbNext),
+                 .up2(wUp2), .down2(wDown2));
+                 
+    debouncer dbR(.clk(clk100MHz), .in(reset), .out(wReset));
+    debouncer dbU(.clk(clk100MHz), .in(up), .out(wUp));
+    debouncer dbD(.clk(clk100MHz), .in(down), .out(wDown));
+    debouncer dbU2(.clk(clk100MHz), .in(up2), .out(wUp2));
+    debouncer dbD2(.clk(clk100MHz), .in(down2), .out(wDown2));
+    
+    // rgb buffer
+    always @(posedge clk100MHz)
+        if(wPTick)
+            rgbReg <= rgbNext;
+            
+    assign rgb = rgbReg;
+    
 endmodule
-
