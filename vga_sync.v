fe3ca3fb71cc0d6fc2bc8c4481138a6928d5bf06
Saeedmohameds <144555221+Saeedmohameds@users.noreply.github.com>
2024-12-01 23:42:51 +0200

    Update vga_sync.v

--- vga_sync.v
+++ vga_sync.v
@@ -1,110 +1,88 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 11/26/2024 03:28:50 PM
-// Design Name: 
-// Module Name: vga_sync
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
 
- 
 module vga_sync(
-    input clk_100MHz,   
+    input clk100MHz,   
     input reset,        
-    output video_on,    
+    output videoOn,    
     output hsync,       
     output vsync,       
-    output p_tick,     
+    output pTick,     
     output [9:0] x,     
     output [9:0] y      
     );
     
-   
- parameter HD = 640;             
+    parameter HD = 640;             
     parameter HF = 48;              
-    parameter HB = 16;            
-    parameter HR = 96;              
-    parameter HMAX = HD+HF+HB+HR-1; 
+    parameter HB = 16;             
+    parameter HR = 96;             
+    parameter HMAX = HD + HF + HB + HR - 1; 
     parameter VD = 480;             
     parameter VF = 10;              
-    parameter VB = 33;               
-    parameter VR = 2;                
-    parameter VMAX = VD+VF+VB+VR-1;   
+    parameter VB = 33;             
+    parameter VR = 2;               
+    parameter VMAX = VD + VF + VB + VR - 1;   
 
-    reg  [1:0] r_25MHz;
-	wire w_25MHz;
-	
-	always @(posedge clk_100MHz or posedge reset)
-		if(reset)
-		  r_25MHz <= 0;
-		else
-		  r_25MHz <= r_25MHz + 1;
-	
-	assign w_25MHz = (r_25MHz == 0) ? 1 : 0; // assert tick 1/4 of the time
+    reg [1:0] r25MHz;
+    wire w25MHz;
     
-   
-    reg [9:0] h_count_reg, h_count_next;
-    reg [9:0] v_count_reg, v_count_next;
+    always @(posedge clk100MHz or posedge reset)
+        if (reset)
+            r25MHz <= 0;
+        else
+            r25MHz <= r25MHz + 1;
     
-    reg v_sync_reg, h_sync_reg;
-    wire v_sync_next, h_sync_next;
+    assign w25MHz = (r25MHz == 0) ? 1 : 0; // assert tick 1/4 of the time
+    
+    reg [9:0] hCountReg, hCountNext;
+    reg [9:0] vCountReg, vCountNext;
+    
+    reg vSyncReg, hSyncReg;
+    wire vSyncNext, hSyncNext;
     
     // Register Control
-    always @(posedge clk_100MHz or posedge reset)
-        if(reset) begin
-            v_count_reg <= 0;
-            h_count_reg <= 0;
-            v_sync_reg  <= 1'b0;
-            h_sync_reg  <= 1'b0;
+    always @(posedge clk100MHz or posedge reset)
+        if (reset) begin
+            vCountReg <= 0;
+            hCountReg <= 0;
+            vSyncReg  <= 1'b0;
+            hSyncReg  <= 1'b0;
         end
         else begin
-            v_count_reg <= v_count_next;
-            h_count_reg <= h_count_next;
-            v_sync_reg  <= v_sync_next;
-            h_sync_reg  <= h_sync_next;
+            vCountReg <= vCountNext;
+            hCountReg <= hCountNext;
+            vSyncReg  <= vSyncNext;
+            hSyncReg  <= hSyncNext;
         end
          
-    //Logic for horizontal counter
-    always @(posedge w_25MHz or posedge reset)      // pixel tick
-        if(reset)
-            h_count_next = 0;
+    // Logic for horizontal counter
+    always @(posedge w25MHz or posedge reset)      // pixel tick
+        if (reset)
+            hCountNext = 0;
         else
-            if(h_count_reg == HMAX)                 // end of horizontal scan
-                h_count_next = 0;
+            if (hCountReg == HMAX)                 // end of horizontal scan
+                hCountNext = 0;
             else
-                h_count_next = h_count_reg + 1;         
+                hCountNext = hCountReg + 1;         
   
     // Logic for vertical counter
-    always @(posedge w_25MHz or posedge reset)
-        if(reset)
-            v_count_next = 0;
+    always @(posedge w25MHz or posedge reset)
+        if (reset)
+            vCountNext = 0;
         else
-            if(h_count_reg == HMAX)                 // end of horizontal scan
-                if((v_count_reg == VMAX))           // end of vertical scan
-                    v_count_next = 0;
+            if (hCountReg == HMAX)                 // end of horizontal scan
+                if ((vCountReg == VMAX))           // end of vertical scan
+                    vCountNext = 0;
                 else
-                    v_count_next = v_count_reg + 1;
+                    vCountNext = vCountReg + 1;
         
-    assign h_sync_next = (h_count_reg >= (HD+HB) && h_count_reg <= (HD+HB+HR-1));
-        assign v_sync_next = (v_count_reg >= (VD+VB) && v_count_reg <= (VD+VB+VR-1));
-        assign video_on = (h_count_reg < HD) && (v_count_reg < VD); // 0-639 and 0-479 
-            
-    assign hsync  = h_sync_reg;
-    assign vsync  = v_sync_reg;
-    assign x      = h_count_reg;
-    assign y      = v_count_reg;
-    assign p_tick = w_25MHz;
+    assign hSyncNext = (hCountReg >= (HD + HB) && hCountReg <= (HD + HB + HR - 1));
+    assign vSyncNext = (vCountReg >= (VD + VB) && vCountReg <= (VD + VB + VR - 1));
+    assign videoOn = (hCountReg < HD) && (vCountReg < VD); // 0-639 and 0-479 
+    
+    assign hsync = hSyncReg;
+    assign vsync = vSyncReg;
+    assign x = hCountReg;
+    assign y = vCountReg;
+    assign pTick = w25MHz;
             
 endmodule
